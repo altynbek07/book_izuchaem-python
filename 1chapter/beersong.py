9eef5445bc153673f64d656758e8f26c9b4c18f1
altynbek07 <altynbek.kazezov.97@gmail.com>
2020-03-12 01:29:35 +0500

    reformat

--- 1chapter/beersong.py
+++ 1chapter/beersong.py
@@ -1,17 +1,17 @@
 word = "bootles"
 
 for beer_num in range(99, 0, -1):
-  print(beer_num, word, "of beer on the wall.")
-  print(beer_num, word, "of beer.")
-  print("Take one down.")
-  print("Pass it around.")
+    print(beer_num, word, "of beer on the wall.")
+    print(beer_num, word, "of beer.")
+    print("Take one down.")
+    print("Pass it around.")
 
-  if beer_num == 1:
-    print("No more bootles of beer on the wall.")
-  else:
-    new_num = beer_num - 1
-    if new_num == 1:
-      word = "bottle"
-    print(new_num, word, "of beer on the wall.")
+    if beer_num == 1:
+        print("No more bootles of beer on the wall.")
+    else:
+        new_num = beer_num - 1
+        if new_num == 1:
+            word = "bottle"
+        print(new_num, word, "of beer on the wall.")
 
-  print()
+    print()
