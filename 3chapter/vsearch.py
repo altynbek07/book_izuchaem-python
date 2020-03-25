99a04de774a6ad6748190dedb34c98b3bdb9bfec
altynbek07 <altynbek.kazezov.97@gmail.com>
2020-03-25 16:55:06 +0500

    improve vsearch

--- 3chapter/vsearch.py
+++ 3chapter/vsearch.py
@@ -1,4 +1,9 @@
-def search4vowels(word: str) -> set:
-    """Выводит гласные, найденные в указанном слове."""
+def search4vowels(phrase: str) -> set:
+    """Возвращает гласные, найденные в указанном слове."""
     vowels = set('aeiou')
-    return vowels.intersection(set(word))
+    return vowels.intersection(set(phrase))
+
+
+def search4letters(phrase: str, letters: str = 'aeiou') -> set:
+    """Возвращает множество букв из 'letters', найденных в указанной фразе."""
+    return set(letters).intersection(set(phrase))
