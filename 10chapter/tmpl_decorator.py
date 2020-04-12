6070c5895aa59c167196648c8f55fc115894dca2
altynbek07 <altynbek.kazezov.97@gmail.com>
2020-04-12 19:27:29 +0500

    decorator

---
+++ 10chapter/tmpl_decorator.py
@@ -0,0 +1,14 @@
+from functools import wraps
+
+
+def decorator_name(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        # 1. Код для выполнения ПЕРЕД вызовом декорируемрй функции.
+
+        # 2. Вызов декорируемой функции и возврат
+        # полученных от нее результатов.
+        return func(*args, **kwargs)
+
+        # 3. Код для выполнения ВМЕСТО вызова декорируемой функции.
+    return wrapper
